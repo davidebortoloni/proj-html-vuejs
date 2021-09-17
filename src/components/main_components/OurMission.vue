8f69cd0af18b32a29dc8c1f43bf1f62305325e43
davidebortoloni <davidebortoloni@gmail.com>
2021-09-17 16:49:09 +0200

    Completameno sezione "OurMission"

--- src/components/main_components/OurMission.vue
+++ src/components/main_components/OurMission.vue
@@ -2,8 +2,25 @@
   <div id="our-mission">
     <div class="container-fluid">
       <div class="row align-items-center">
-        <div class="col-7"></div>
-
+        <div class="col-7">
+          <div class="images w-100">
+            <div class="back">
+              <img
+                class="w-100"
+                src="@/assets/img/busy-architect-PYVKWM4-1024x872.jpg"
+                alt="Architect"
+              />
+              <div class="overlay"></div>
+            </div>
+            <div class="front">
+              <img
+                class="w-100"
+                src="@/assets/img/businesswoman-analysing-document-P8WSNMC-1024x820.jpg"
+                alt="Business Woman"
+              />
+            </div>
+          </div>
+        </div>
         <div class="col-5">
           <h2>
             learn more about our
@@ -35,4 +52,39 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../../assets/scss/_vars.scss";
+
+.images {
+  position: relative;
+  top: 40px;
+  right: 40px;
+  display: flex;
+  justify-content: center;
+  .front,
+  .back {
+    border-radius: 20px;
+    overflow: hidden;
+    width: 80%;
+    box-shadow: 3px 3px 15px 0 $sixthColor;
+  }
+  .back {
+    position: absolute;
+    top: -80px;
+    left: 0;
+    .overlay {
+      position: absolute;
+      z-index: 1;
+      opacity: 0.8;
+      top: 0px;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(90deg, $secondaryColor, $mainColor);
+    }
+  }
+  .front {
+    position: relative;
+    z-index: 1;
+  }
+}
 </style>
